0071c349de0d9e7134670445d5b1640eb99eb783
LaelPierre <lael.aichaoui@gmail.com>
2022-10-22 10:41:17 -0700

    js added selectors inside element method

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 // traversing the DOM method
 // select element
-const btns = document.querySelectorAll(".question-btn");
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// // functionnality
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+
+//     question.classList.toggle("show-text");
+//   });
+// });
+
+// using selectors inside element method
+// select element
+const questions = document.querySelectorAll(".question");
 
 // functionnality
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const question = e.currentTarget.parentElement.parentElement;
+questions.forEach(function (question) {
+  // console.log(question);
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
 
     question.classList.toggle("show-text");
   });
